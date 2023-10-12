888a5faf87433b28858c78c2de643b4cdea92681
wongzc <52870701+wongzc@users.noreply.github.com>
2023-10-13 07:30:42 +0800

    update

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {motion, AnimatePresence} from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion';
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
