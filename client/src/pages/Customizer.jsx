656fcaab46ae426ad744a8be486223a51981b82d
wongzc <52870701+wongzc@users.noreply.github.com>
2023-10-20 07:26:17 +0800

    update customizer function

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -34,12 +34,45 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />
       case "filepicker":
-        return <FilePicker />
+        return <FilePicker 
+        file={file}
+        setFile={setFile}
+        />
       case "aipicker":
         return <AIPicker/>
       default:
         return null
     }
+  }
+
+  const handleDecals=(type,result) =>{
+    const decalType =DecalTypes[type]
+
+    state[decalType.stateProperty] =result
+
+    if(!activeFilterTab[decalType.filterTab]){
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+  const handleActiveFilterTab =(tabName) =>{
+    switch (tabName) {
+      case "logoshirt":
+        state.isLogoTexture=!activeFilterTab[tabName]
+        break;
+      case "stylishShirt":
+        state.isFullTexture =!activeFilterTab[tabName]
+      default:
+        state.isLogoTexture = true
+        state.isFullTexture =false
+        
+    }
+  }
+  const readFile=(type)=> {
+    reader(file)
+    .thern((result)=>{
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    })
   }
   return (
     <AnimatePresence>
