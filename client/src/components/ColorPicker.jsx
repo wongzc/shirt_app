5794d5c7951955a6e7cacbbec13e4b778af09f19
wongzc <52870701+wongzc@users.noreply.github.com>
2023-10-20 07:05:13 +0800

    updated color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {SketchPicker} from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state)
   return (
-    <div>
-      colorpick
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+      color={snap.color}/>
     </div>
   )
 }
